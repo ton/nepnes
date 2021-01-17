960d6271bf56f26696f89c94a0148185be7f6f60
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2021-01-17 16:16:01 +0100

    Improve scrolling; add status line
    The textual assembly is now only printed once to one big notcurses
    plane, which is subsequently moved around to simulate scrolling. This is
    simpler (and faster?) than reprinting all of the assembly every time.
    
    Adds a simple status line with a very basic help screen.
    
    When manually inputting an address to jump to, backspace actually works.

--- src/tools/dbg/dbg.c
+++ src/tools/dbg/dbg.c
@@ -3,16 +3,12 @@
 #include "cpu.h"
 
 /*
- * Scrolls the assembly viewport by the given number of lines. Requires the CPU
- * state to convert an assembly line number to an address in memory.
+ * Scrolls the debugger by the given number of lines.
  */
-void dbg_scroll_assembly(struct Debugger *debugger, struct Cpu *cpu, int lines)
+void dbg_scroll_assembly(struct Debugger *debugger, int lines)
 {
-  int current_line = cpu_instruction_count(cpu, debugger->assembly_address);
-  int line = current_line + lines;
-  if (line < 0) line = 0;
-
-  debugger->assembly_address = cpu_find_instruction_address(cpu, line);
+  debugger->line += lines;
+  if (debugger->line < 0) debugger->line = 0;
 }
 
 /*
@@ -25,6 +21,8 @@ void dbg_scroll_assembly_to_address(struct Debugger *debugger, struct Cpu *cpu,
   /* We can not simply assign the requested address to the debugger state; but
    * need to normalize it such that the debugger address is set to the
    * instruction that starts or overlaps the requested address. */
-  debugger->assembly_address =
+  Address assembly_address =
       cpu_find_instruction_address(cpu, cpu_instruction_count(cpu, address));
+
+  debugger->line = cpu_instruction_count(cpu, address);
 }

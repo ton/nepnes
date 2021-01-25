ef95282534b9ff5ad4c3caeed7f608b20132d685
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2021-01-25 22:39:38 +0100

    dbg: major improvements
    Scrolling in the debugger is now done in the same way as it was done in
    the initial version; for every change in memory location, the data that
    needs to be printed is recalculated again instead of loaded from a
    cache. For now, this is more than fast enough, and it prevents issues
    where the program writes to memory and the debugger does not see it,
    because it cached the whole assembly view...ooops.
    
    Furthermore, the debugger now only interprets a subset of memory as
    assembly; that part covered by the loaded ROM (for now). In the future,
    surely this needs to be adapted in case of self-modifying code etc., but
    for now this works and prevents issues when programs start storing data
    in zero page memory for example.
    
    This adds emulation for a whole bunch of branch instructions to the CPU.
    
    Add a new command 'f', which will bring the focus of the assembly window
    back to the program counter.
    
    Fixes a small formatting issue in the Nintendulator log.

--- src/tools/dbg/dbg.c
+++ src/tools/dbg/dbg.c
@@ -1,15 +1,81 @@
 #include "dbg.h"
 
 #include "cpu.h"
+#include "instruction.h"
 
 /*
  * Initializes a debugger, given the CPU state, focuses the debugger on the
  * instruction the program counter points to.
  */
-void dbg_init(struct Debugger *debugger, struct Cpu *cpu)
+void dbg_init(struct Debugger *debugger, struct Cpu *cpu, uint16_t prg_offset,
+              size_t prg_size)
 {
-  debugger->last_line = cpu_instruction_count(cpu, CPU_MAX_ADDRESS);
   debugger->scroll_offset = 3;
+  debugger->prg_offset = prg_offset;
+  debugger->prg_size = prg_size;
+  debugger->last_line = dbg_address_to_line(debugger, cpu, CPU_MAX_ADDRESS);
+
+  dbg_scroll_assembly_to_pc(debugger, cpu);
+}
+
+/*
+ * Converts a memory address to a line number in the debugger. The line number
+ * will display the data at the given address, or that overlaps the given
+ * address.
+ */
+int dbg_address_to_line(struct Debugger *debugger, struct Cpu *cpu,
+                        uint16_t address)
+{
+  /* Each data memory location is displayed on a single line. In case we are in
+   * the data segment, just return the address as the line number. */
+  if (address <= debugger->prg_offset || debugger->prg_size == 0)
+  {
+    return address;
+  }
+
+  uint8_t *first = cpu->ram + debugger->prg_offset;
+  uint8_t *last = cpu->ram + address;
+
+  int line = debugger->prg_offset;
+  while (first < last)
+  {
+    first += instruction_size(*first);
+    ++line;
+  }
+
+  /* In case we jumped past the requested address, the address is contained
+   * within an instruction, and we have to compensate. */
+  return first > last ? line - 1 : line;
+}
+
+/*
+ * Converts a memory address to a line number in the debugger. The line number
+ * will display the data at the given address, or that overlaps the given
+ * address.
+ */
+uint16_t dbg_line_to_address(struct Debugger *debugger, struct Cpu *cpu,
+                             int line)
+{
+  /* Each data memory location is displayed on a single line. In case we are in
+   * the data segment, just return the address as the line number. */
+  if (line <= debugger->prg_offset || debugger->prg_size == 0)
+  {
+    return line;
+  }
+
+  uint8_t *first = cpu->ram + debugger->prg_offset;
+  uint8_t *last = cpu->ram + sizeof(cpu->ram) + 1;
+
+  int curr_line = debugger->prg_offset;
+  while (curr_line < line && first < last)
+  {
+    first += instruction_size(*first);
+    ++curr_line;
+  }
+
+  /* In case we jumped past the requested address, the address is contained
+   * within an instruction, and we have to compensate. */
+  return (first > last ? last : first) - cpu->ram;
 }
 
 /*
@@ -31,7 +97,10 @@ void dbg_scroll_assembly_to_address(struct Debugger *debugger, struct Cpu *cpu,
                                     Address address)
 {
   debugger->line =
-      cpu_instruction_count(cpu, address) - debugger->scroll_offset;
+      dbg_address_to_line(debugger, cpu, address) - debugger->scroll_offset;
+  if (debugger->line < 0) debugger->line = 0;
+  if (debugger->line > debugger->last_line)
+    debugger->line = debugger->last_line;
 }
 
 /*
@@ -40,6 +109,5 @@ void dbg_scroll_assembly_to_address(struct Debugger *debugger, struct Cpu *cpu,
  */
 void dbg_scroll_assembly_to_pc(struct Debugger *debugger, struct Cpu *cpu)
 {
-  debugger->line =
-      cpu_instruction_count(cpu, cpu->PC) - debugger->scroll_offset;
+  dbg_scroll_assembly_to_address(debugger, cpu, cpu->PC);
 }
--- src/tools/dbg/dbg.h
+++ src/tools/dbg/dbg.h
@@ -8,14 +8,26 @@
 /* Stores state of the debugger. */
 struct Debugger
 {
-  int line;          /* current focused line of assembly to display */
-  int pc_line;       /* line of assembly the PC is pointing to */
-  int last_line;     /* line number of the last line of assembly */
-  int scroll_offset; /* minimal number of lines to keep above and below the PC
-                        line */
+  int line;              /* current focused line of assembly to display */
+  int last_line;         /* line number of the last line of assembly */
+  uint8_t scroll_offset; /* minimal number of lines to keep above and below the
+                            PC line */
+
+  /* The following is used by the debugger which parts of the memory should be
+   * interpreted as program data. This may not be sufficient! */
+
+  uint16_t prg_offset; /* start of the program data */
+  size_t prg_size;     /* size of the program data */
 };
 
-void dbg_init(struct Debugger *debugger, struct Cpu *cpu);
+void dbg_init(struct Debugger *debugger, struct Cpu *cpu, uint16_t prg_address,
+              size_t prg_data_size);
+
+int dbg_address_to_line(struct Debugger *debugger, struct Cpu *cpu,
+                        uint16_t address);
+uint16_t dbg_line_to_address(struct Debugger *debugger, struct Cpu *cpu,
+                             int line);
+
 void dbg_scroll_assembly(struct Debugger *debugger, int lines);
 void dbg_scroll_assembly_to_address(struct Debugger *debugger, struct Cpu *cpu,
                                     Address address);

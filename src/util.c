e5592d9003e6fce02ecf83687a9fcefd0ea61ebc
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2021-02-27 20:27:52 +0100

    Add get_timestamp
    For some poor mans profiling this comes in handy from time to time.

--- src/util.c
+++ src/util.c
@@ -151,6 +151,14 @@ void quit_strerror(const char *fmt, ...)
 
 static void quit_optional_strerror(bool append_strerror, const char *fmt,
                                    va_list ap)
+/* Returns a timestamp with microseconds precision. */
+timestamp_t get_timestamp()
+{
+  struct timeval now;
+  gettimeofday(&now, NULL);
+  return now.tv_usec + (timestamp_t)now.tv_sec * 1000000;
+}
+
 {
   char buf[MAXLINE];
   vsnprintf(buf, MAXLINE - 1, fmt, ap);

cc274154e91f689531ad76d962096b7fe90dda6e
Ton van den Heuvel <git@tonvandenheuvel.com>
2022-02-15 16:09:39 +0100

    sandbox: print correct build type in Zsh prompt

--- init-sandbox.sh
+++ init-sandbox.sh
@@ -1,2 +1,8 @@
 export SANDBOX_NAME=nepnes
 export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
+
+sandbox_details_fn() {
+  if [ -f build/CMakeCache.txt ]; then
+    sed -n 's/^CMAKE_BUILD_TYPE:STRING=\(.*\)$/\L\1/p' build/CMakeCache.txt
+  fi
+}
